067f00534bdf2a61400f69a96257eb78b385a858
Dmitry Klochkov <Klochkov.Dm595@yandex.ru>
2021-09-15 01:30:11 +0300

    Получаем погоду из интернета

--- app/src/main/java/com/example/weatherapp/view/DetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/DetailsFragment.kt
@@ -1,50 +1,115 @@
 package com.example.weatherapp.view
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.example.weatherapp.BuildConfig
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.DetailsFragmentBinding
 import com.example.weatherapp.model.data.Weather
+import com.example.weatherapp.model.data.dto.WeatherDTO
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
+
+private const val API_KEY = BuildConfig.WEATHER_API_KEY
 
 class DetailsFragment: Fragment() {
 
     private var _binding: DetailsFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = DetailsFragmentBinding.inflate(inflater,container,false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let{weather -> populateData(weather)}
+        weatherBundle = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?: Weather()
+            binding.main.hide()
+            binding.loadingLayout.show()
+            loadWeather()
     }
 
-        private fun populateData (weatherData: Weather) {
-            with(binding) {
-                weatherData.city.also { city ->
-                    cityName.text = city.city
-                    cityCoordinates.text = String.format(
-                        getString(R.string.city_coordinates),
-                        city.lat.toString(),
-                        city.lon.toString()
-                    )
-                }
-                weatherData.apply {
-                    temperatureValue.text = temperature.toString()
-                    feelsLikeValue.text = feelsLike.toString()
-                }
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadWeather() {
+        try {
+            val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${weatherBundle.city.lat}&lon=${weatherBundle.city.lon}")
+            val handler = Handler(Looper.myLooper()!!)
+            Thread {
+                var urlConnection: HttpsURLConnection? = null
+                try {
+                    urlConnection = uri.openConnection() as HttpsURLConnection
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.addRequestProperty("X-Yandex-API-Key", API_KEY)
+                    urlConnection.readTimeout = 10000
 
-            }
+                    val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val result = getLines(reader)
+                    val weatherDTO:WeatherDTO = Gson().fromJson(result, WeatherDTO::class.java)
+
+
+                    handler.post {
+                        displayWeather(weatherDTO)
+                    }
+                } catch (e: Exception) {
+                    Log.e("WETHER", "Fail connection", e)
+                    e.printStackTrace()
+                } finally {
+                    urlConnection?.disconnect()
+                }
+            }.start()
+        } catch (e: MalformedURLException) {
+            Log.e("WEATHER", "Fail URI", e)
+            e.printStackTrace()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
+    }
+
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            main.show()
+            loadingLayout.hide()
+            weatherBundle.city.also { city ->
+                cityName.text = city.city
+                cityCoordinates.text = String.format(
+                    getString(R.string.city_coordinates),
+                    city.lat.toString(),
+                    city.lon.toString()
+                )
+            }
+            weatherDTO.fact?.let { fact ->
+                temperatureValue.text = fact.temp.toString()
+                feelsLikeValue.text = fact.feels_like.toString()
+                weatherCondition.text = fact.condition
+            }
+
+        }
+    }
 
     companion object{
         const val BUNDLE_EXTRA = "weather"
